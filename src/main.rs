b587cb19fbb566734e0b9ca20d175e2dccfd5616
Scott Windsor <swindsor@pivotal.io>
2017-04-13 17:03:41 -0700

    Get repl loop working for Lvals
    Also added regex crate.
    
    Signed-off-by: Andres Medina <amedina@pivotal.io>

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,51 @@
 use std::io::{self, Write, Error};
-use std::process::exit;
 
-const QUIT: &'static str = "quit";
+extern crate regex;
 
-fn read() -> Result<String,Error> {
+enum Line {
+    Quit,
+    Lval(Lval),
+}
+
+enum Lval {
+    Number(i32),
+    Atom(String),
+    String(String),
+}
+
+fn read() -> Result<Line, Error> {
     let mut input = String::new();
     print!("> ");
     io::stdout().flush().unwrap();
 
     match io::stdin().read_line(&mut input) {
-        Ok(0) => Ok(QUIT.to_string()),
-        Ok(_) => Ok(input.trim().to_string()),
-        Err(e) => Err(e)
+        Ok(0) => Ok(Line::Quit),
+        Ok(_) if input.trim() == "quit" => Ok(Line::Quit),
+        Ok(_) => Ok(Line::Lval(parse(input.trim()))),
+        Err(e) => Err(e),
     }
 }
 
-fn eval(s: &str) -> &str {
-    s
+fn parse(s: &str) -> Lval {
+    let re = regex::Regex::new(r#""(\w*)""#).unwrap();
+    if re.is_match(s) {
+        let cap = re.captures(s).unwrap();
+        Lval::String(cap[1].into())
+    } else {
+        match i32::from_str_radix(s, 10) {
+            Ok(i) => Lval::Number(i),
+            _ => Lval::Atom(s.into()),
+        }
+    }
+}
+
+fn eval(l: &Lval) -> String {
+    match *l {
+        Lval::Atom(ref s) => format!("atom: {}", s),
+        Lval::String(ref s) => format!("string: \"{}\"", s),
+        Lval::Number(i) => format!("number: {}", i),
+    }
+
 }
 
 fn print(s: &str) {
@@ -28,12 +57,12 @@ fn main() {
 
     loop {
         match read() {
-            Ok(ref s) if s == QUIT => {
+            Ok(Line::Quit) => {
                 println!("Bye!");
-                exit(0);
+                break;
             }
-            Ok(ref s) =>  print(eval(s)),
-            Err(e) => println!("Error: {}", e)
+            Ok(Line::Lval(l)) => print(&eval(&l)),
+            Err(e) => println!("Error: {}", e),
         }
     }
 }
